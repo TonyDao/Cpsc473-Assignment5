b1a5dae65ddc7db5e51cd01bfe23051a3ac1a32d
Tony Dao <sangdao2014@csu.fullerton.edu>
2016-11-14 00:25:04 -0800

    modify front end display

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,10 +1,48 @@
 var main = function() {
     'use strict';
 
+    //check player enter username
+    $('#loginButton').on('click', function(e) {
+        e.preventDefault();
+        var username = $('#username').val();
+        if(username != '') {
+            //disable login-form
+            $('#login-form').transition({
+                animation: 'fade up',
+                onComplete: function() {
+                    //show trivia question game
+                    $('#game-form').transition('fade down');
+                    //display username on header
+                    $('#game-form h2 span').html(username);
+                }
+            });            
+        }
+    });
+
     var answerId = 1;
 
     //enable tab selection
-    $('.tabular.menu .item').tab();
+    var previousTab = $('.ui.tab.active.segment');
+    $('.vertical.pointing.menu .item').tab({
+        'onLoad': function(e){
+            var currentTab = $('.ui.tab.active.segment');
+            //hide current and show prevous to allow animate
+            currentTab.hide();
+            previousTab.show();
+
+            //animate to hide prevous tab
+            previousTab.transition({
+                animation: 'scale',
+                //show current tab
+                onComplete: function() {
+                    currentTab.transition('scale');
+                }
+            });
+
+            //set current tab to be previous for feature animate
+            previousTab = currentTab;
+        }
+    });
 
     //get random question
     var getQuestion = function() {
@@ -14,55 +52,64 @@ var main = function() {
                 answerId = data.answerId;
             } else {
                 console.log('No question');
-            }            
+            }
         });
     };
 
     //get next question
     $('#getQuestionButton').on('click', function() {
+        //prevent form to reload page
+        $('.ui.form.answer').form({
+            onSuccess: function() {
+                return false;
+            }
+        });
         getQuestion();
     });
 
-    //answer question form valication
-    $('.ui.form.answer').form({
-        fields: {
-            answer: {
-                identifier  : 'answer',
-                rules: [
-                    {
-                        type   : 'empty',
-                        prompt : 'Please enter your question answer'
-                    }
-                ]
-            }
-        }, 
-        onSuccess: function(){
-            var answer = $('#answer').val();
-            var result;
-
-            var jsonData = JSON.stringify({'answer': answer, 'answerId': answerId});
-
-            $.ajax({
-                url     : '/answer',
-                method  : 'POST',
-                dataType: 'json',
-                contentType: 'application/json',
-                data    : jsonData,
-                success : function(data) {
-                    result = data.correct ? 'True':'False';
-                    $('.ui.segment.answer h3 span').html(result);
-                },
-                error   : function() {
-                    console.log('Error post answer');
+    //answer button clicked
+    $('#answerButton').on('click', function() {
+        //answer question form valication
+        $('.ui.form.answer').form({
+            fields: {
+                answer: {
+                    identifier  : 'answer',
+                    rules: [
+                        {
+                            type   : 'empty',
+                            prompt : 'Please enter your question answer'
+                        }
+                    ]
                 }
-            });            
+            }, 
+            onSuccess: function(){
+                var answer = $('#answer').val();
+                var result;
 
-            //remove question and answer
-            $('#answer').val('');
-            return false;
-        }
+                var jsonData = JSON.stringify({'answer': answer, 'answerId': answerId});
 
+                $.ajax({
+                    url     : '/answer',
+                    method  : 'POST',
+                    dataType: 'json',
+                    contentType: 'application/json',
+                    data    : jsonData,
+                    success : function(data) {
+                        result = data.correct ? 'True':'False';
+                        $('.ui.segment.answer h3 span').html(result);
+                    },
+                    error   : function() {
+                        console.log('Error post answer');
+                    }
+                });            
+
+                //remove question and answer
+                $('#answer').val('');
+                return false;
+            }
+        });
     });
+    
 
     //create question validation
     $('.ui.form.question').form({
